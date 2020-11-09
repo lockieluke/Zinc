0ad590b5e547516e2bd2476d47fce1b557c20280
lockieluke <sherlockluk@outlook.com>
2020-11-09 17:20:45 +0800

    Added hotkeys for Text Field Actions

--- main/ctxMenus/tfActions.ts
+++ main/ctxMenus/tfActions.ts
@@ -2,36 +2,44 @@ import {Menu, MenuItem} from "electron";
 import addMenuItems from "./batchMenuItems";
 
 export default function getTfActions(menu: Menu, textContent: string = "") {
+    if (process.platform === 'win32') {
+        addMenuItems(menu, [
+            new MenuItem({
+                label: "Emoji",
+                sublabel: "Win + Period"
+            }),
+            new MenuItem({
+                type: 'separator'
+            })
+        ])
+    }
     addMenuItems(menu, [
         new MenuItem({
-            label: "Emoji"
+            label: "Undo",
+            accelerator: "CommandOrControl+Z"
         }),
         new MenuItem({
-           type: 'separator'
-        }),
-        new MenuItem({
-            label: "Undo"
-        }),
-        new MenuItem({
-            label: "Undo"
-        }),
-        new MenuItem({
-            label: "Redo"
+            label: "Redo",
+            accelerator: "CommandOrControl+Y"
         }),
         new MenuItem({
             type: 'separator'
         }),
         new MenuItem({
-            label: "Cut"
+            label: "Cut",
+            accelerator: "CommandOrControl+X"
         }),
         new MenuItem({
-            label: "Copy"
+            label: "Copy",
+            accelerator: "CommandOrControl+C"
         }),
         new MenuItem({
-            label: "Paste"
+            label: "Paste",
+            accelerator: "CommandOrControl+V"
         }),
         new MenuItem({
-            label: "Select All"
+            label: "Select All",
+            accelerator: "CommandOrControl+A"
         }),
     ])
 }
