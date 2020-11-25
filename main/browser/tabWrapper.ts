fe97462c43f9df7a123017e1fe65a3820a761662
lockieluke <sherlockluk@outlook.com>
2020-11-25 12:27:33 +0800

    Made wrapper for Tab Management for the main process

---
+++ main/browser/tabWrapper.ts
@@ -0,0 +1,16 @@
+import {getCurrentWindow} from "./winCtrls";
+import {currentBV} from "./tabMng";
+
+export class TabWrapper {
+    public static newTab(url: string): void {
+        getCurrentWindow().webContents.send('tabwrapper-newtab', url);
+    }
+
+    public static closeTab(): void {
+        getCurrentWindow().webContents.send('tabwrapper-closetab');
+    }
+
+    public static navigate(url: string) {
+        currentBV.webContents.loadURL(url);
+    }
+}
