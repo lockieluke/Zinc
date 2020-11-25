05dad78d580132936dccd92ca22bbdeaaeda316d
lockieluke <sherlockluk@outlook.com>
2020-11-25 13:23:30 +0800

    Added functions for TF Actions

--- main/ctxMenus/tfActions.ts
+++ main/ctxMenus/tfActions.ts
@@ -1,12 +1,15 @@
-import {Menu, MenuItem} from "electron";
+import {app, Menu, MenuItem} from "electron";
 import addMenuItems from "./batchMenuItems";
 
-export default function getTfActions(menu: Menu, textContent: string = "") {
+export default function getTfActions(menu: Menu, params: Electron.ContextMenuParams) {
     if (process.platform === 'win32') {
         addMenuItems(menu, [
             new MenuItem({
                 label: "Emoji",
-                sublabel: "Win + Period"
+                sublabel: "Win + Period",
+                click: function () {
+                    app.showEmojiPanel();
+                }
             }),
             new MenuItem({
                 type: 'separator'
@@ -16,30 +19,41 @@ export default function getTfActions(menu: Menu, textContent: string = "") {
     addMenuItems(menu, [
         new MenuItem({
             label: "Undo",
-            accelerator: "CommandOrControl+Z"
+            accelerator: "CommandOrControl+Z",
+            role: "undo"
         }),
         new MenuItem({
             label: "Redo",
-            accelerator: "CommandOrControl+Y"
+            accelerator: "CommandOrControl+Y",
+            role: 'redo'
         }),
         new MenuItem({
             type: 'separator'
         }),
         new MenuItem({
             label: "Cut",
-            accelerator: "CommandOrControl+X"
+            accelerator: "CommandOrControl+X",
+            role: 'cut'
         }),
         new MenuItem({
             label: "Copy",
-            accelerator: "CommandOrControl+C"
+            accelerator: "CommandOrControl+C",
+            role: 'copy'
         }),
         new MenuItem({
             label: "Paste",
-            accelerator: "CommandOrControl+V"
+            accelerator: "CommandOrControl+V",
+            role: 'pasteAndMatchStyle'
+        }),
+        new MenuItem({
+            label: "Paste as plain text",
+            accelerator: "CommandOrControl+Shift+V",
+            role: 'paste'
         }),
         new MenuItem({
             label: "Select All",
-            accelerator: "CommandOrControl+A"
+            accelerator: "CommandOrControl+A",
+            role: 'selectAll'
         }),
     ])
 }
