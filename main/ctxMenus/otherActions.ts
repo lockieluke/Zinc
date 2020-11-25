be1f134a35cd6707e37fcd4b9617bda0d6f74c01
lockieluke <sherlockluk@outlook.com>
2020-11-25 12:25:10 +0800

    Implemented click events for most actions for Other Actions CTX Menu

--- main/ctxMenus/otherActions.ts
+++ main/ctxMenus/otherActions.ts
@@ -1,37 +1,72 @@
-import {Menu, MenuItem} from "electron";
+import {app, dialog, Menu, MenuItem} from "electron";
+import {currentBV} from './../browser/tabMng'
 import addMenuItems from "./batchMenuItems";
+import {getCurrentWindow} from "../browser/winCtrls";
+import * as path from "path";
 
 export default function getOtherActions(menu: Menu) {
     addMenuItems(menu, [
         new MenuItem({
             label: "Back",
-            accelerator: "Alt+Right"
+            accelerator: "Alt+Right",
+            enabled: currentBV.webContents.canGoBack(),
+            click: function () {
+                currentBV.webContents.goBack();
+            }
         }),
         new MenuItem({
             label: "Forward",
-            accelerator: "Alt+Left"
+            accelerator: "Alt+Left",
+            enabled: currentBV.webContents.canGoForward(),
+            click: function () {
+                currentBV.webContents.goForward();
+            }
         }),
         new MenuItem({
             label: "Reload",
-            accelerator: "CommandOrControl+R"
+            accelerator: "CommandOrControl+R",
+            click: function () {
+                currentBV.webContents.reload();
+            }
         }),
         new MenuItem({
             type: 'separator'
         }),
         new MenuItem({
             label: "Save as...",
-            accelerator: "CommandOrControl+S"
+            accelerator: "CommandOrControl+S",
+            click: function () {
+                dialog.showSaveDialog(getCurrentWindow(), {
+                    title: "Save Page",
+                    defaultPath: path.join(app.getPath('documents'), 'index.html'),
+                    filters: [{
+                        name: "HTML (Complete Webpage)",
+                        extensions: ['html']
+                    }]
+                }).then(result => {
+                    currentBV.webContents.savePage(result.filePath, "HTMLComplete");
+                })
+            }
         }),
         new MenuItem({
             label: "Print...",
-            accelerator: "CommandOrControl+P"
+            accelerator: "CommandOrControl+P",
+            click: function () {
+                currentBV.webContents.print({}, function (success, failureReason) {
+                    if (!success)
+                        console.log(failureReason);
+                })
+            }
         }),
         new MenuItem({
             type: 'separator'
         }),
         new MenuItem({
             label: "View page source",
-            accelerator: "CommandOrControl+U"
+            accelerator: "CommandOrControl+U",
+            click: function () {
+
+            }
         })
     ])
 }
