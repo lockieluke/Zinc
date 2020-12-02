2441c0428ba52e8d0d8132255cda7d39fecb63e4
lockieluke <sherlockluk@outlook.com>
2020-12-02 16:01:38 +0800

    Added keystrokes for opening new tabs and closing tabs

--- main/keystrokes/tabActions.ts
+++ main/keystrokes/tabActions.ts
@@ -1,9 +1,19 @@
 import electronLocalKeystroke from "./index";
+import TabWrapper from "../browser/tabWrapper";
+import zincProtocolHandler from './../../window/protocol'
 
 export default function registerTabActionsKeystrokes(window: Electron.BrowserWindow) {
     electronLocalKeystroke.registerLocalKeyStroke('CommandOrControl+Shift+Alt+I', window, function () {
         window.webContents.openDevTools({
             mode: 'undocked'
         });
+    })
+
+    electronLocalKeystroke.registerLocalKeyStroke('CommandOrControl+T', window, function () {
+        TabWrapper.newTab(zincProtocolHandler('zinc://newtab'));
+    })
+
+    electronLocalKeystroke.registerLocalKeyStroke('CommandOrControl+W', window, function () {
+        TabWrapper.closeTab();
     })
 }
