577da09f5eb41c55c6b96711f136c61f64d0fd28
lockieluke <sherlockluk@outlook.com>
2020-12-08 10:15:38 +0800

    Added function for getting Java path and added Native components

---
+++ main/native/java.ts
@@ -0,0 +1,19 @@
+import * as path from "path";
+import * as child_process from "child_process";
+
+export function getJavaPath(appModule: Electron.App): string {
+    if (!appModule.isPackaged) {
+        return path.join(require.main.path, '..', 'java', 'bin', 'java.exe');
+    } else {
+        return path.join(Electron.app.getPath('exe'), 'java');
+    }
+}
+
+export function runJar(javaPath: string, jarPath: string, argument: string, callback?: (stdout: string, stderr: string) => any): void {
+    child_process.exec(`${javaPath} -jar ${jarPath} ${argument}`, {
+        cwd: path.join(require.main.path, '..'),
+        env: process.env,
+    }, function (error, stdout, stderr) {
+        callback(stdout, stderr);
+    })
+}
