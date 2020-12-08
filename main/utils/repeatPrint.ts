c70193ddd978c75c781147acdb189800f1133886
lockieluke <sherlockluk@outlook.com>
2020-12-08 09:34:57 +0800

    Added function to easily print array into console

---
+++ main/utils/repeatPrint.ts
@@ -0,0 +1,7 @@
+export function repeatPrintArray(array: Array<any>) {
+    if (array != null) {
+        for (let i = 0; i < array.length; i++) {
+            console.log(array[i]);
+        }
+    }
+}
