7126b00e4e853bc2baa563947eb7d140c53f6fb4
lockieluke <sherlockluk@outlook.com>
2020-12-28 12:06:55 +0800

    New Zinc Native startup procedure

--- main/native/java.ts
+++ main/native/java.ts
@@ -1,19 +1,21 @@
-import * as path from 'path';
-import * as child_process from 'child_process';
+import * as path from "path";
+import * as child_process from "child_process";
+import getAppRoot from "../utils/appPath";
 
 export function getJavaPath(appModule: Electron.App): string {
-    if (!appModule.isPackaged) {
-        return path.join(require.main.path, '..', 'java', 'bin', 'java.exe');
-    } else {
-        return path.join(path.dirname(appModule.getPath('exe')), 'java', 'bin', 'java.exe');
-    }
+  if (!appModule.isPackaged) {
+    return path.join(require.main.path, "..", "java", "bin", "java.exe");
+  } else {
+    return path.join(path.dirname(appModule.getPath("exe")), "java", "bin", "java.exe");
+  }
 }
 
-export function runJar(javaPath: string, jarPath: string, argument?: string, callback?: (stdout: string, stderr: string) => any): void {
-    child_process.exec(`${javaPath} -jar -Xmx20M ${jarPath}` + (argument == undefined ? '' : ` ${argument}`), {
-        cwd: path.join(require.main.path, '..'),
-        env: process.env,
-    }, function (error, stdout, stderr) {
-        callback(stdout, stderr);
-    })
+export function runJar(javaPath: string, jarPath: string, argument?: string, errCallback?: (stderr: string) => void, outCallback?: (stdout: string) => void, quitCallback?: (exitcode: number | null) => void): void {
+  const javaProcess = child_process.exec(`${javaPath} -jar -Xmx20M ${jarPath}` + (argument == undefined ? "" : ` ${argument}`), {
+    env: process.env,
+    cwd: getAppRoot()
+  });
+  javaProcess.stdout.on("data", data => outCallback(data));
+  javaProcess.stderr.on("data", data => errCallback(data));
+  javaProcess.on("exit", exitcode => quitCallback(exitcode));
 }
--- main/native/zincNative.ts
+++ main/native/zincNative.ts
@@ -1,6 +1,9 @@
 import NativeCommunication from "./communication";
 import * as path from "path";
 import getAppRoot from "../utils/appPath";
+import { getJavaPath, runJar } from "./java";
+import defaultLogger from "../logger/";
+import { LogLevel, LogTypes } from "../logger";
 
 interface DiscordRPC {
     zincNative: ZincNative
@@ -10,19 +13,36 @@ interface DiscordRPC {
 export default class ZincNative {
 
     private nativeCommunication: NativeCommunication;
-    private app: Electron.App;
+    private readonly app: Electron.App;
 
     public constructor(nativeCommuniction: NativeCommunication, app: Electron.App) {
         this.nativeCommunication = nativeCommuniction;
         this.app = app;
     }
 
-    public DiscordRPC: DiscordRPC = {
-        zincNative: <ZincNative>(this as ZincNative),
-        changeRPCDescription: function (description: string): void {
-            this.zincNative.nativeCommunication.getWS().send(`custom:ChangeRPCDescription:${description}`);
+    public startup(): void {
+        // Windows is the only platform which supports Zinc Native for now
+        if (process.env.NO_NATIVE_JAR !== "true" && process.platform === "win32") {
+            const jarPath: string = this.getJarPath(), javaPath: string = getJavaPath(this.app);
+            defaultLogger(LogTypes.ZincNative, `Starting bundled Zinc Native which is placed in ${jarPath} with JVM in ${javaPath}`, LogLevel.Log);
+            runJar(javaPath, jarPath, "", function(stderr) {
+                defaultLogger(LogTypes.ZincNative, stderr, LogLevel.Error);
+            }, function(stdout) {
+                defaultLogger(LogTypes.ZincNative, stdout);
+            }, function(exitcode) {
+                defaultLogger(LogTypes.ZincNative, exitcode.toString(), LogLevel.Info);
+            });
+        } else {
+            defaultLogger(LogTypes.ZincNative, "Zinc Native is currently not supported on other platforms", LogLevel.Info);
         }
     }
+
+    public DiscordRPC: DiscordRPC = {
+        zincNative: <ZincNative>(this as ZincNative),
+        changeRPCDescription: function(description: string): void {
+            this.zincNative.nativeCommunication.getWS().send(`custom:ChangeRPCDescription:${description}`);
+        }
+    };
 
     public getJarPath(): string {
         if (!this.app.isPackaged) {
