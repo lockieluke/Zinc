da384d1e2ea21397db190c5da9a5e371d5161fbd
lockieluke <sherlockluk@outlook.com>
2020-12-28 21:32:56 +0800

    Fixed code for packaged application

--- main/native/java.ts
+++ main/native/java.ts
@@ -1,25 +1,26 @@
-import * as path from "path";
-import * as child_process from "child_process";
-import getAppRoot from "../utils/appPath";
+import * as path from 'path';
+import * as child_process from 'child_process';
+import getAppRoot from '../utils/appPath';
 
 export function getJavaPath(appModule: Electron.App): string {
   if (!appModule.isPackaged) {
-    return path.join(require.main.path, "..", "java", "bin", "java.exe");
+    return path.join(getAppRoot(), 'java', 'bin', 'java.exe');
   } else {
-    return path.join(path.dirname(appModule.getPath("exe")), "java", "bin", "java.exe");
+    return path.join(path.dirname(appModule.getPath('exe')), 'java', 'bin', 'java.exe');
   }
 }
 
-export function runJar(javaPath: string, jarPath: string, argument?: string, errCallback?: (stderr: string) => void, outCallback?: (stdout: string) => void, quitCallback?: (exitcode: number | null) => void): void {
+export function runJar(javaPath: string, jarPath: string, appModule: Electron.App, argument?: string, errCallback?: (stderr: string) => void, outCallback?: (stdout: string) => void, quitCallback?: (exitcode: number | null) => void): void {
   const javaArgs: string[] = ['-jar', '-Xmx20M', jarPath];
   if (argument != undefined || argument != '')
     javaArgs.push(argument);
 
   const javaProcess = child_process.spawn(javaPath, javaArgs, {
     env: process.env,
-    cwd: getAppRoot()
+    cwd: appModule.isPackaged ? path.dirname(appModule.getPath('exe')) : getAppRoot(),
   });
-  javaProcess.stdout.on("data", data => outCallback(data));
+  console.log(appModule.isPackaged ? appModule.getAppPath() : getAppRoot());
+  javaProcess.stdout.on('data', data => outCallback(data));
   javaProcess.stderr.on("data", data => errCallback(data));
   javaProcess.on("exit", exitcode => quitCallback(exitcode));
 }
